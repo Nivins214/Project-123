8b7fba2d2e7735788e3f06cbf4bb1b9db346803b
Nivins214 <79670863+Nivins214@users.noreply.github.com>
2022-10-14 19:50:53 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function setup(){
+video=createCapture(VIDEO);
+video.size(550,500);
+canvas=createCanvas(550,550);
+canvas.position(560,150);
+posenet=ml5.poseNet(video,modelloaded);
+posenet.on('pose',gotposes);
+}
+
+function modelloaded(){
+    console.log('posenet is initialized');
+}
+
+function gotposes(results){
+    if(results.length>0){
+        console.log(results);
+
+    }
+}
+
+function draw(){
+    background('#969A97');
+}
